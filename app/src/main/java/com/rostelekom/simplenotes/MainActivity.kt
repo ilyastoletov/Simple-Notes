e873134c25d91c8522264c2237e89b5d55d05768
Ilya <ilastoletov7@gmail.com>
2022-11-27 18:25:43 +0300

    added notes rv + applied binding

--- app/src/main/java/com/rostelekom/simplenotes/MainActivity.kt
+++ app/src/main/java/com/rostelekom/simplenotes/MainActivity.kt
@@ -2,10 +2,17 @@ package com.rostelekom.simplenotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.rostelekom.simplenotes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main).apply { lifecycleOwner = this@MainActivity }
+        supportActionBar!!.title = "Notes"
     }
+
 }
